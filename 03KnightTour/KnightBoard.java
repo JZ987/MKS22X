c190b9bad86ae33360393b3c99b1d5e1f73836d6
Jack Zheng <jzheng7@stuy.edu>
2017-02-15 15:35:19 -0500

    Work on the solveH method

--- 03KnightTour/KnightBoard.java
+++ 03KnightTour/KnightBoard.java
@@ -5,7 +5,7 @@ public class KnightBoard{
     private int col;
 
     public KnightBoard(int startingRow, int startingCol){
-	board = new int[startingRow][startingCol];\
+	board = new int[startingRow][startingCol];
 	row = startingRow;
 	col = startingCol;
     }
@@ -24,8 +24,8 @@ public class KnightBoard{
 
     public String toString(){
 	String temp = "";
-	for(int row = 0; row < board.length; row++){
-	    for(int col = 0; col < board[0].length; col++){
+	for(int row = 0; row < this.row; row++){
+	    for(int col = 0; col < this.col; col++){
 		if(board[row][col] < 10){
 		    temp += " " + board[row][col] + " ";
 		}else{
@@ -37,4 +37,8 @@ public class KnightBoard{
 	return temp;
     }
     
+    public static void main(String[] args){
+	KnightBoard a = new KnightBoard(4, 5);
+	System.out.println(a);
+    }
 }
