f2c1aa00522d6329dff943cd2802cb66b63e4ecb
Jack Zheng <jzheng7@stuy.edu>
2017-02-28 15:32:14 -0500

    Modify Recursion to be better suited for grading

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -4,23 +4,29 @@ public class Recursion{
 	return "Zheng,Jack";
     }
 
-    public static double sqrt(double n) throws IllegalArgumentException{
+    public static double sqrt(double n){
+	if(n < 0){
+	    throw new IllegalArgumentException();
+	}
 	return helper(n, 1);
     }
 
     private static double helper(double n, double guess){
-	if(Math.abs(guess * guess - n) <= 0.00000000001)
-	return guess;
+	if(n == 0){
+	    return 0;
+	}else if(Math.abs(guess * guess - n) <= 0.00000001){
+	    return guess;
+	}
 	double better_guess = (n / guess + guess) / 2;
 	return helper(n, better_guess);
     }
 
     public static void main(String[] args){
 
-	System.out.println("Square root of " + 121 + " is: " + sqrt(121));
+	System.out.println("Square root of " + 121 + " is: " + sqrt(121.0));
 	System.out.println("Square root of " + 125 + " is: " + sqrt(125));
 	System.out.println("Square root of " + 625 + " is: " + sqrt(625));
-	System.out.println("Square root of " + 50 + " is: " + sqrt(50));	
+	System.out.println("Square root of " + 50 + " is: " + sqrt(50));
     }
 }
 	    
