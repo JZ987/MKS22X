3fa0a6567c0d74328cbb3e57fce4f594f7f5648e
Jack Zheng <jzheng7@stuy.edu>
2017-03-03 15:33:46 -0500

    Work on maze in class

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -5,11 +5,13 @@ public class Maze{
     private char[][] maze;
     private int row = 0;
     private int col = 0;
-    private String file;
+    private String file = "";
     
     public Maze(String fileName){
 	scanFile(fileName);
-	System.out.println(file);
+	//System.out.println(file);
+	//System.out.println(row);
+	//System.out.println(col);
 	initializeMaze();
     }
 
@@ -18,15 +20,41 @@ public class Maze{
 	    Scanner inf = new Scanner(new File(fileName));
 	    while(inf.hasNextLine()){
 		row++;
-		String line = inf.nextLine();
-		System.out.println(line);
-	    }       
+		file += inf.nextLine() + "\n";
+	    }
+	    col = file.length() / row - 1;
+	}catch(FileNotFoundException e){
+	    System.out.println("File is not found.");
+	}
+    }
+    
+    private void initializeMaze(){
+	int x = 0;
+	maze = new char[row][col];
+	for(int i = 0; i < row; i++){
+	    for(int j = 0; j < col; j++){
+		maze[i][j] = file.charAt(x);
+		x++;
+	    }
+	}
     }
 
-    private void initializeMaze(){
+    public String toString(){
+	String temp = "";
+	for(int i = 0; i < row; i++){
+	    for(int j = 0; j < col; j++){
+		temp += maze[i][j];
+	    }
+	}
+	return temp;
     }
 
     public static void main(String args[]){
 	Maze b = new Maze("data1.dat");
+	Maze c = new Maze("data2.dat");
+	Maze d = new Maze("data3.dat");
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
     }
 }
