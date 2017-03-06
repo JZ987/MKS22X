1b7b70dc129d9182807cc5f3c1ce162d0c5ba663
Jack Zheng <jzheng7@stuy.edu>
2017-03-05 20:26:33 -0500

    Complete maze.java

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -5,13 +5,15 @@ public class Maze{
     private char[][] maze;
     private int row = 0;
     private int col = 0;
+    private int startr = -1;
+    private int startc = -1;
     private String file = "";
+    private boolean animate;
+    private boolean valid;
     
     public Maze(String fileName){
+	animate = false;
 	scanFile(fileName);
-	//System.out.println(file);
-	//System.out.println(row);
-	//System.out.println(col);
 	initializeMaze();
     }
 
@@ -20,9 +22,9 @@ public class Maze{
 	    Scanner inf = new Scanner(new File(fileName));
 	    while(inf.hasNextLine()){
 		row++;
-		file += inf.nextLine() + "\n";
+		file += inf.nextLine();
 	    }
-	    col = file.length() / row - 1;
+	    col = file.length() / row;
 	}catch(FileNotFoundException e){
 	    System.out.println("File is not found.");
 	}
@@ -30,31 +32,85 @@ public class Maze{
     
     private void initializeMaze(){
 	int x = 0;
+	int valid = 0;
 	maze = new char[row][col];
 	for(int i = 0; i < row; i++){
 	    for(int j = 0; j < col; j++){
+		if(file.charAt(x) == 'S'){
+		    startr = i;
+		    startc = j;
+		    valid++;
+		}
+		if(file.charAt(x) == 'E'){
+		    valid++;
+		}
 		maze[i][j] = file.charAt(x);
 		x++;
 	    }
 	}
+	if(valid == 2){
+	    this.valid = true;
+	}else{
+	    this.valid = false;
+	}
     }
 
+    public boolean solve(){
+	if(!valid){
+	    System.out.println("This maze lack 'S' or 'E'");
+	    return false;
+	}else{
+	    maze[startr][startc] = ' ';
+	    return solve(startr, startc);
+	}
+    }
+
+    private boolean solve(int row, int col){
+	if(animate){
+	    System.out.println("\033[2J\033[1;1H" + this);
+	    wait(30);
+	}
+	if(maze[row][col] == 'E'){
+	    return true;
+	}
+	if(maze[row][col] != ' '){
+	    return false;
+	}
+	maze[row][col] = '@';
+	if(solve(row, col + 1) ||
+	   solve(row, col - 1) ||
+	   solve(row - 1, col) ||
+	   solve(row + 1, col)){
+	    return true;
+	}
+	maze[row][col] = '.';
+	return false;
+    }
+    
+    private void wait(int millis){
+	try {
+             Thread.sleep(millis);
+         }
+         catch (InterruptedException e) {
+         }
+    }
+
+    public void setAnimate(boolean b){
+	animate = b;
+    }
+
+    public void clearTerminal(){
+	System.out.println("\033[2J\033[1;1H");
+    }
+    
     public String toString(){
 	String temp = "";
 	for(int i = 0; i < row; i++){
 	    for(int j = 0; j < col; j++){
 		temp += maze[i][j];
 	    }
+	    temp += "\n";
 	}
 	return temp;
-    }
-
-    public static void main(String args[]){
-	Maze b = new Maze("data1.dat");
-	Maze c = new Maze("data2.dat");
-	Maze d = new Maze("data3.dat");
-	System.out.println(b);
-	System.out.println(c);
-	System.out.println(d);
     }
 }
