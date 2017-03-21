f6fa2bab4a97e247710a849400b021d3ca0f6f80
Jack Zheng <jzheng7@stuy.edu>
2017-03-21 15:34:06 -0400

    Work on quickselect and quicksort

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,63 +1,76 @@
 import java.util.Random;
 
 public class Quick{
-    private static int answer;
-    private static int[] data;
 
-    public Quick(int[] array){
-	answer = 0;
-	data = array;
+    public static void quicksort(int[] array){
+	
     }
 
-    public static int quickselect(int k, int start, int end){
-	int index = partition(start, end);
-	if(index == k){
-	    return answer;
+    public static int quickselect(int[] array, int k){
+	if(quickSelectHelper(array, k, 0, array.length - 1)){
+	    return array[k];
 	}
-	return quicksort(k, start, end);
     }
 
-    public static int partition(int start, int end){
+    private static boolean quickSelectHelper(int[] array, int k, int start, int end){
 	int index = new Random().nextInt(end - start) + start;
-	int pivot = data[index];
-	swap(index, end);
-	int count = 0;
-	int[] temp = new int[(end + 1) - start];
-	for(int i = 0; i < temp.length; i++){
-	    temp[i] = data[start + i];
-	}
-	for(int i = 0; i < temp.length; i++){
-	    if(temp[i] == pivot){
-		count++;
-	    }
-	    if(temp[i] < pivot){
-		data[start + i] = temp[i];
-		start++;
-	    }
-	    if(temp[i] > pivot){
-		//temp[end] = data[i];
-		end--;
+	int pivot = array[index];
+	swap(array, start, pivot);
+	int lt = start;
+	int i = start + 1;
+	int gt = end;
+	while(i <= gt){
+	    if(array[i] == pivot){
+		i++;
+	    }else if(array[i] < pivot){
+		swap(array, lt, i);
+		lt++;
+		i++;
+	    }else if(array[i] > pivot){
+		swap(array, i, gt);
+		gt--;
 	    }
 	}
-	for(int i = start; i < count; i++){
-	    start++;
+	swap(array, start, lt);
+	if(lt <= k && k >= gt){
+	    return true;
 	}
-	answer = pivot;
-	//temp[start] = number;
-	return start;
+	return quickSelectHelper(array, k);
     }
 
-    private static void swap(int index1, int index2){
-	int firstDigit = data[index1];
-	int secondDigit = data[index2];
-	data[index1] = secondDigit;
-	data[index2] = firstDigit;
+    public static int partition(int[] array, int start, int end){
+	int index = new Random().nextInt(end - start) + start;
+	int pivot = array[index];
+	swap(array, start, pivot);
+	int lt = start;
+	int i = start + 1;
+	int gt = end;
+	while(i <= gt){
+	    if(array[i] == pivot){
+		i++;
+	    }else if(array[i] < pivot){
+		swap(array, lt, i);
+		lt++;
+		i++;
+	    }else if(array[i] > pivot){
+		swap(array, i, gt);
+		gt--;
+	    }
+	}
+	swap(array, start, lt);
+	return lt;
+    }
+
+    private static void swap(int[] array, int index1, int index2){
+	int firstDigit = array[index1];
+	int secondDigit = array[index2];
+	array[index1] = secondDigit;
+	array[index2] = firstDigit;
     }
 
     public static void main(String[] args){
 
 	int[] array = {2, 10, 15, 23, 0, 5};
-	Quick a = new Quick(array);
 	
 	System.out.println(a.quickselect(0, 0, 5));
 	System.out.println(a.quickselect(1, 0, 5));
