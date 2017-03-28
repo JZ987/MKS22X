a3b2862c170473228c6a70317823200c8f8337b2
Jack Zheng <jzheng7@stuy.edu>
2017-03-28 15:34:16 -0400

    Work on MyLinkedList.java, complete add and get.

--- 09List/MyLinkedList.java
+++ 09List/MyLinkedList.java
@@ -3,7 +3,7 @@ public class MyLinkedList{
     private LNode start, end, current;
     private int size;
 
-    public MyLinkedList{
+    public MyLinkedList(){
 	start = null;
 	end = null;
 	current = start;
@@ -12,17 +12,50 @@ public class MyLinkedList{
 
     public boolean add(int value){
 	if(size == 0){
-	    current = new LNode(value);
-	    start = current;
-	    end = current;
+	    start = new LNode(value);
+	    end = start;
+	    current = start;
+	    size++;
 	    return true;
 	}
-
-	current.next = new LNode(value);
+	current = end;
+	current.setNext(value);
 	end = current.getNext();
 	size++;
 	return true;
     }
+
+    public int get(int index){
+	current = start;
+	while(index != 0){
+	    index--;
+	    current = current.getNext();
+	}
+	return current.getValue();
+    }
+
+    public int set(int index, int newValue){
+	
+    }
+
+    public int size(){
+	return size;
+    }
+
+    public String toString(){
+	String temp = "[";
+	current = start;
+	while(current.getNext() != null){
+	    temp += current.getValue() + ", ";
+	    current = current.getNext();
+	}
+	temp += current.getValue() + "]";
+	return temp;
+    }
+
+
+
+
 
     class LNode{
 	
@@ -34,8 +67,33 @@ public class MyLinkedList{
 	    next = null;
 	}
 
+	public LNode getPrev(){
+	    return prev;
+	}
+
 	public LNode getNext(){
 	    return next;
 	}
+
+	public void setNext(int value){
+	    next = new LNode(value);
+	}
+
+	public int getValue(){
+	    return value;
+	}
+    }
+
+
+    public static void main(String[] args){
+	MyLinkedList hello = new MyLinkedList();
+	hello.add(7);
+	hello.add(3);
+	hello.add(5);
+	hello.add(6);
+	hello.add(13);
+	hello.add(-4);
+	System.out.println(hello.get(3));
+	System.out.println(hello);
     }
 }
