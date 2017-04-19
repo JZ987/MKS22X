1a96a0bf95a0b7d3fd1d7c18e3188826cf1cf48e
Jack Zheng <jzheng7@stuy.edu>
2017-04-18 22:18:04 -0400

    Start on deque

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class MyDeque{
+
+    public void addFirst(String newString) throws NullPointerException{
+
+    }
+
+    public void addLast(String newString) throws NullPointerException{
+
+    }
+
+    public String removeFirst(){
+	return "";
+    }
+
+    public String removeLast(){
+	return "";
+    }
+
+    public String getFirst(){
+	return "";
+    }
+
+    public String getLast(){
+	return "";
+    }
+
+}
