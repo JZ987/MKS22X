f84dce98fc8a97ed9ed66bc8ed20a8a696518096
Jack Zheng <jzheng7@stuy.edu>
2017-04-26 13:08:30 -0400

    Complete eval method and its helper methods

--- 11StackCalculations/StackCalc.java
+++ 11StackCalculations/StackCalc.java
@@ -1,4 +1,42 @@
+import java.util.Stack;
+
 public class StackCalc{
+
+    public static double eval(String equation){
+	String[] tokens = equation.split(" ");
+	Stack<Double> values = new Stack<Double>();
+	for(String token : tokens){
+	    if(isOp(token)){
+		values.push(perform(token, values.pop(), values.pop()));
+	    }else{
+		values.push(Double.parseDouble(token));
+	    }
+	}
+	return values.pop();
+    }
+
+    private static boolean isOp(String token){
+	return token.equals("+") ||
+   	       token.equals("-") ||
+	       token.equals("*") ||
+	       token.equals("/") ||
+	       token.equals("%");
+    }
+
+    private static double perform(String token, double digit_1, double digit_2){
+	if(token.equals("+")){
+	    return digit_2 + digit_1;
+	}else if(token.equals("-")){
+	    return digit_2 - digit_1;
+	}else if(token.equals("*")){
+	    return digit_2 * digit_1;
+	}else if(token.equals("/")){
+	    return digit_2 / digit_1;
+	}else if(token.equals("%")){
+	    return digit_2 % digit_1;
+	}
+	return 0;
+    }
 
     public static void main(String[] args){
 	System.out.println(StackCalc.eval("10 2 +"));//12.0
