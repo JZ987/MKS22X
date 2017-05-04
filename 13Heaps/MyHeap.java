82bb8c8915414a45663f1dad57dcc6e20b80df82
Jack Zheng <jzheng7@stuy.edu>
2017-05-04 15:33:49 -0400

    Need more working on remove

--- 13Heaps/MyHeap.java
+++ 13Heaps/MyHeap.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.lang.IndexOutOfBoundsException;
+import java.util.NoSuchElementException;
 
 public class MyHeap{
     
@@ -30,7 +30,7 @@ public class MyHeap{
 	pushUp();
     }
 
-    public String remove(){
+    public String remove() throws NoSuchElementException{
 	String removed = heap.get(1);
 	heap.set(1, heap.remove(size));
 	size--;
@@ -38,37 +38,38 @@ public class MyHeap{
 	return removed;
     }
 
-    public String peek(){
-	String temp = "[";
-	for(String s : heap){
-	    temp += s + " ";
-	}
-	return temp + "]";
+    public String peek() throws NoSuchElementException{
+	return heap.get(1);
     }
 
     private void pushUp(){
 	int n = size;
-	System.out.println("Input: " + peek());
-	while(n != 1 && Integer.parseInt(heap.get(n)) >= Integer.parseInt(heap.get(n/2))){
+	while(n != 1 && heap.get(n).compareTo(heap.get(n/2)) > 0){
 	    swap(n, n/2);
 	    n /= 2;
 	}
-	System.out.println("Output: " + peek());
     }
 
-    private void pushDown() throws IndexOutOfBoundsException{
+    private void pushDown(){
 	int n = 1;
-	System.out.println("Input: " + peek());
-	while(Integer.parseInt(heap.get(n)) < Integer.parseInt(heap.get(n*2)) ||
-	      Integer.parseInt(heap.get(n)) < Integer.parseInt(heap.get(n*2+1))){
-	    System.out.println("Process: " + peek());
-	    if(Integer.parseInt(heap.get(n*2)) > Integer.parseInt(heap.get(n*2+1))){
-		swap(n, n*2);
-		n *= 2;
+	while(n*2 <= size || n*2+1 <= size){
+	    if(n*2+1 <= size){
+		if(heap.get(n).compareTo(heap.get(n*2)) <= 0 && 
+		   heap.get(n).compareTo(heap.get(n*2+1)) <= 0){
+		    if(heap.get(n*2).compareTo(heap.get(n*2+1)) > 0){
+			swap(n, n*2);
+			n *= 2;
+		    }else{
+			swap(n, n*2+1);
+			n *= 2;
+			n += 1;
+		    }
+		}
 	    }else{
-		swap(n, n*2+1);
-		n *= 2;
-		n += 1;
+		if(heap.get(n).compareTo(heap.get(n*2)) < 0){
+		    swap(n, n*2);
+		    n *= 2;
+		}
 	    }
 	}
     }
@@ -79,18 +80,26 @@ public class MyHeap{
 	heap.set(second, temp);
     }
 
+    public String toString(){
+	String temp = "[";
+	for(String s : heap){
+	    temp += s + " ";
+	}
+	return temp + "]";
+    }
+
     public static void main(String[] args){
 	MyHeap test = new MyHeap();
-	test.add("10");
-	test.add("5");
-	test.add("8");
-	test.add("3");
-	test.add("20");
-	test.add("15");
-	test.add("17");
-	test.add("1232");
-	test.add("-1");
+	test.add("a");
+	test.add("b");
+	test.add("c");
+	test.add("d");
+	test.add("e");
+	test.add("f");
+	test.add("g");
+	test.add("h");
+	test.add("i");
 	test.remove();
-	System.out.println(test.peek());
+	System.out.println(test);
     }
 }
