9fff5716f354e1e77b6bfbf131d8e2686873d1a4
Jack Zheng <jzheng7@stuy.edu>
2017-05-18 15:32:51 -0400

    Work on MazeSolver. Start on instantiating different frontiers

--- 15MazeSolver/MazeSolver.java
+++ 15MazeSolver/MazeSolver.java
@@ -1,4 +1,57 @@
 public class MazeSolver{
 
+    private Maze board;
+    private Frontier method;
+    private boolean animate;
+
+    public MazeSolver(String fileName){
+	this(fileName, false);
+    }
+
+    public MazeSolver(String fileName, boolean animate){
+	board = new Maze(fileName);
+	this.animate = animate;
+    }
     
+    public void solve(){
+	solve(1);
+    }
+    
+    public void solve(int style){
+	switch (style){
+	case 0: depthSearch();
+	    break;
+	case 1: breadthSearch();
+	    break;
+	case 2: bestFirstSearch();
+	    break;
+	case 3: aStarSearch();
+	    break;
+	default: System.out.println("Invalid solve method");
+	    break;
+	}
+    }
+
+    private void depthSearch(){
+	method = new FrontierStack();
+	method.add(board.getStart());
+	
+    }
+
+    private void breadthSearch(){
+
+    }
+
+    private void bestFirstSearch(){
+
+    }
+
+    private void aStarSearch(){
+
+    }	 
+
+    private void process(Location current){
+	
+
+    }
 }
